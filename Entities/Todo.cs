37c4b58927aa83f482b5c04423e829188b50e764
Enrico Joe <enjoe21@outlook.com>
2024-07-09 09:31:37 +0700

    Add project files.

---
+++ Entities/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Todo
+    {
+        public int id { get; set; }
+        public string nama { get; set; }
+        public bool urgent { get; set; }
+        public bool done { get; set; }
+    }
+}
